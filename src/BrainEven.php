3cef62a189b2c69100c5f8232960f3328b7e2e6f
Alexander Rysaev <alex.rysaev@yandex.com>
2020-03-25 14:03:42 +0000

    Written logic of Brain Even game

---
+++ src/BrainEven.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BrainGames\BrainEven;
+
+use function cli\line;
+use function cli\prompt;
+
+function isEven($number) {
+    return $number % 2 == 0;
+}
+
+function brainEven($name) {
+    $isCorrect = true;
+    for ($i = 0; $i < 3; $i++) {
+        $number = rand(1, 100);
+        $answer = isEven($number) ? 'yes' : 'no';
+        line("Question: %s", $number);
+        $guess = prompt('Your answer');
+        $isCorrect = $guess == $answer ? true : false;
+        if ($isCorrect) {
+            line("Correct!");
+        } else {
+            line("Incorrect!");
+            break;
+        }
+    }
+    if ($isCorrect) {
+        line("Congratulations, %s!", $name);
+    }
+}
--- src/Cli.php
+++ src/Cli.php
@@ -4,10 +4,16 @@ namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
+use function BrainGames\BrainEven\brainEven;
 
 function run()
 {
-    line('Welcome to the Brain Game!');
+    line();
+    line('Welcome to the Brain Games!');
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    line();
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    line();
+    brainEven($name);
 }
